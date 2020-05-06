6093a7508b0a2f8b84a6abb19a05221ff1ab20c8
itokeso <61177741+itokeso@users.noreply.github.com>
2020-05-06 16:51:19 +0900

    drill

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,9 @@
 class GroupsController < ApplicationController
 
+  def index
+
+  end
+  
   def new
     @group = Group.new
     @group.users << current_user
@@ -13,6 +17,20 @@ class GroupsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+    redirect_to root_path, notice: 'グループを更新しました'
+    else
+    render :edit
+    end
+  end
+  
 
   private
   def group_params
